f30181912dbfe9b6e5273f9ede13d2df0e94a62a
Ichinose Shogo <shogo82148@gmail.com>
2015-03-29 10:27:32 +0900

    引数のテスト

--- mono-test/Program.cs
+++ mono-test/Program.cs
@@ -4,6 +4,23 @@ namespace monotest
 {
 	class MainClass
 	{
+		void Foo (int fuga = 10)
+		{
+		}
+
+		void Foo (string hoge, int fuga = 10)
+		{
+		}
+
+		public static void Main ()
+		{
+		}
+
+		void Bar()
+		{
+			Foo (fuga: 20);
+		}
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
